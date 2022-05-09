2f3e67aff28b19a99f6e99f0e407c4e34fa31477
Ballard Ingram <ballardingram@outlook.com>
2022-05-09 10:51:43 -0500

    first commit, install and directory set up

---
+++ index.js
@@ -0,0 +1,5 @@
+// EXTERNAL PACKAGES > REQUIRED FOR FUNCTIONALITY
+// INSTALLED > NODE (https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+// INSTALLED > MYSQL2 (https://www.npmjs.com/package/mysql2)
+// INSTALLED > INQUIRER (https://www.npmjs.com/package/inquirer)
+// INSTALLED > CONSOLE.TABLE (https://www.npmjs.com/package/console.table)
