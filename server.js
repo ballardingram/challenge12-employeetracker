c243b1ac6c804099c1e2c7c8fb16fd8a51c2cdc0
Ballard Ingram <ballardingram@outlook.com>
2022-05-09 12:11:04 -0500

    server.js - set up server and port file, done

--- server.js
+++ server.js
@@ -3,3 +3,29 @@
 // INSTALLED > MYSQL2 (https://www.npmjs.com/package/mysql2)
 // INSTALLED > INQUIRER (https://www.npmjs.com/package/inquirer)
 // INSTALLED > CONSOLE.TABLE (https://www.npmjs.com/package/console.table)
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
+// PORT > STANDARD SYNTAX FOR PORT
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use('/api', apiRoutes);
+
+// MIDDLEWARE > EXPRESS
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// ERROR ROUTES > DEFAULT REQUEST NOT FOUND - ANY OTHER
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// PORT > START SERVER ANTE DB CONNECTION
+db.connect(err => {
+    if (err) throw err;
+    console.log('Databse connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}!`);
+    });
+});
