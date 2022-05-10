d8ce37618cc6697cff820dbf39179d306324b65a
Ballard Ingram <ballardingram@outlook.com>
2022-05-10 14:16:55 -0500

    schema.sql - refractored and adjusted syntax, working on seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,7 +17,7 @@ VALUES
 ('Director of Customer Service', '12500000', 3);
 
 -- SEEDS > EMPLOYEE
-INSERT INTO employee (first_name, last_name, role_id, manager_id )
+INSERT INTO employee (first_name, last_name, roles_id, manager_id )
 VALUES
 ('Candace', 'Sure', 1, NULL),
 ('Anna', 'Lorn', 2, 1),
@@ -27,3 +27,21 @@ VALUES
 ('Caleb', 'Antonio', 6, NULL),
 ('Bo', 'Walling', 7, 6);
 
+-- SEEDS > MY SQL FUNCTIONS - ALL DEPARTMENTS
+SELECT department_id, department.name FROM department ORDERED BY department.id;
+
+-- SEEDS > MYSQL FUNCTIONS - DEPARTMENT - JOIN FUNCTIONS
+-- DOCUMENTATION > JOIN (https://www.w3schools.com/mysql/mysql_join.asp)
+SELECT department.name AS department, employee.id, employee.first_name, employee.last_name, roles.title
+FROM employee
+LEFT JOIN department ON (department.id = roles.department_id)
+LEFT JOIN roles ON (role.id = employee.roles_id)
+ORDER BY department.name;
+
+-- SEEDS > MYSQL FUNCTIONS - ROLE - JOIN FUNCTIONS
+-- SAME DOCUMENTATION AS ABOVE
+SELECT roles.title, employee.first_name, employee.last_name, department.name AS department
+FROM employee
+LEFT JOIN roles ON (roles.id = employee.roles_id)
+LEFT JOIN department ON (department.id = roles.department_id)
+ORDER BY roles.title;
